ff3e5d348076c196fbe5f511d20bde66c83b4c29
cesarromerodev <cesar_romero.dev@outlook.com>
2022-04-19 17:15:21 -0500

    Seguimiento 208

--- history.md
+++ history.md
@@ -1,5 +1,12 @@
 # add [12-04-2022]
+
     - Fin de la sección 12 HeroesApp
     - Creacion de Rutas.
+
 # add [13-04-2022]
+
     - Seguimiento seccion 13 , 196
+
+# add [19-04-2022]
+
+    - Seguimiento seccion 14 , 208
--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Pipe } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 
 import { HeroesService } from '../../services/heroes.service';
@@ -8,7 +8,12 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `
   ]
 })
 export class HeroeComponent implements OnInit {
@@ -16,12 +21,22 @@ export class HeroeComponent implements OnInit {
 
   constructor(
     private heroesService: HeroesService,
-    private activateRoute: ActivatedRoute
+    private activateRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
+  /**
+   * ActivatedRoute:
+   * Brinda acceso a información sobre una ruta asociada a un componente
+   *  que se carga en un tomacorriente. Úselo para recorrer el RouterStateárbol
+   *  y extraer información de los nodos.
+   */
   ngOnInit(): void {
     this.activateRoute.params.pipe(switchMap(({ id }) => this.heroesService.getHeroePorId(id)))
       .subscribe(heroe => this.heroe = heroe);  //para traer respuesta del id params
+  }
+  regresar() {
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
